bc55a4b88a4882696dcaedb1b9f9cd21f608116f
Mikhail Sushkov <sushkovm@gmail.com>
2014-05-13 16:08:56 -0700

    need to debug bm25 and tune params; starting code for smallest window

--- src/edu/stanford/cs276/BM25Scorer.java
+++ src/edu/stanford/cs276/BM25Scorer.java
@@ -65,8 +65,6 @@ public class BM25Scorer extends AScorer
 		avgLengths = new HashMap<String,Double>();
 		pagerankScores = new HashMap<Document,Double>();
 
-		// TODO: DEBUG
-		
 		//normalize avgLengths
 		for (Query q : queryDict.keySet()) {
 			for (String url : queryDict.get(q).keySet()) {
@@ -76,14 +74,14 @@ public class BM25Scorer extends AScorer
 				if (!pagerankScores.containsKey(curr)) {
 					pagerankScores.put(curr, Math.log10(curr.page_rank));
 				}
-				
+
 				Map<String, Double> currDoc = new HashMap<String, Double>();
 
 				for (String tfType : this.TFTYPES) {
 					if (!avgLengths.containsKey(tfType)) {
 						avgLengths.put(tfType, 0.0);
 					}
-					
+
 					int currLength = 0;
 
 					if (tfType.equals("url")) {
@@ -103,18 +101,19 @@ public class BM25Scorer extends AScorer
 							currLength += getNumTokens(anchor) * curr.anchors.get(anchor);
 						}
 					}
-					
+
 					avgLengths.put(tfType, avgLengths.get(tfType) + currLength);
 
 					currDoc.put(tfType, (double) currLength);
 				}
-				
+
 				lengths.put(curr, currDoc);
 			}
 		}
-		
+
+		// have the sums for each type; now compute the avg using # docs 
 		for (String type : this.TFTYPES) {
-			avgLengths.put(type, avgLengths.get(type) / )
+			avgLengths.put(type, avgLengths.get(type) / lengths.size());
 		}
 
 	}
@@ -122,36 +121,54 @@ public class BM25Scorer extends AScorer
 	////////////////////////////////////
 
 
-	public double getNetScore(Map<String,Map<String, Double>> tfs, Query q, Map<String,Double> tfQuery,Document d)
-	{
+	public double getNetScore(Map<String,Map<String, Double>> tfs, Query q, Map<String,Double> tfQuery,
+			Document d, Map<String,Double> idfs) {
 		double score = 0.0;
 
-		/*
-		 * @//TODO : Your code here
-		 */
+		// for each term in the query
+		for (String term : tfQuery.keySet()) {
+			double w = 0.0;
+			
+			for (String type : tfs.keySet()) {
+				w += weightParams.get(type) * tfs.get(type).get(term);
+			}
+			
+			score += w * idfs.get(term) / (k1 + w);
+		}
 
+		score += pageRankLambda * Math.log10(pageRankLambdaPrime + pagerankScores.get(d));
+		
 		return score;
 	}
 
 	//do bm25 normalization
-	public void normalizeTFs(Map<String,Map<String, Double>> tfs,Document d, Query q)
+	public void normalizeTFs(Map<String, Map<String, Double>> tfs, Document d, Query q)
 	{
-		/*
-		 * @//TODO : Your code here
-		 */
+		for (String type : tfs.keySet()) {
+			for (String term : tfs.get(type).keySet()) {
+				if (avgLengths.get(type) == 0) {
+					tfs.get(type).put(term, 0.0);
+				} else {
+					double newVal = tfs.get(type).get(term);
+					
+					newVal /= 1.0 + weightParams.get("b" + type) * 
+							(lengths.get(d).get(type) / avgLengths.get(type) - 1);
+
+					tfs.get(type).put(term, newVal);
+				}
+			}
+		}
 	}
 
 
 	@Override
 	public double getSimScore(Document d, Query q, Map<String,Double> idfs, int numDocs) throws Exception { 
 		Map<String,Map<String, Double>> tfs = this.getDocTermFreqs(d,q);
-
 		this.normalizeTFs(tfs, d, q);
 
 		Map<String,Double> tfQuery = getQueryFreqs(q);
 
-
-		return getNetScore(tfs,q,tfQuery,d);
+		return getNetScore(tfs, q, tfQuery, d, idfs);
 	}
 
 	private int getNumTokens(String str) {
--- src/edu/stanford/cs276/SmallestWindowScorer.java
+++ src/edu/stanford/cs276/SmallestWindowScorer.java
@@ -1,36 +1,31 @@
 package edu.stanford.cs276;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 //doesn't necessarily have to use task 2 (could use task 1, in which case, you'd probably like to extend CosineSimilarityScorer instead)
-public class SmallestWindowScorer extends BM25Scorer
+public class SmallestWindowScorer extends CosineSimilarityScorer
 {
 
-	/////smallest window specifichyperparameters////////
+	/////smallest window specific hyperparameters////////
     double B = -1;    	    
     double boostmod = -1;
     
     //////////////////////////////
 	
-	public SmallestWindowScorer(Map<String, Double> idfs,Map<Query,Map<String, Document>> queryDict) 
+	public SmallestWindowScorer(Map<String, Double> idfs, Map<Query, Map<String, Document>> queryDict) 
 	{
-		super(idfs, queryDict);
+		super(idfs);
 		handleSmallestWindow();
 	}
 
 	
 	public void handleSmallestWindow()
 	{
-		/*
-		 * @//TODO : Your code here
-		 */
+		
 	}
 
 	
-	public double checkWindow(Query q,String docstr,double curSmallestWindow,boolean isBodyField)
+	public double checkWindow(Query q, String docstr, double curSmallestWindow, boolean isBodyField)
 	{
 		/*
 		 * @//TODO : Your code here
@@ -40,14 +35,14 @@ public class SmallestWindowScorer extends BM25Scorer
 	
 	
 	@Override
-	public double getSimScore(Document d, Query q) {
+	public double getSimScore(Document d, Query q, Map<String,Double> idfs, int numDocs) throws Exception {
 		Map<String,Map<String, Double>> tfs = this.getDocTermFreqs(d,q);
 		
 		this.normalizeTFs(tfs, d, q);
 		
 		Map<String,Double> tfQuery = getQueryFreqs(q);
 		
-		return 0;
+		return getNetScore(tfs, q, tfQuery, d, idfs, numDocs) * B;
 	}
 
 }
